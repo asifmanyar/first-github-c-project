35839ae5db9026f6eab6487cb8dcd8dd71ab8574
asifmanyar <121081798+asifmanyar@users.noreply.github.com>
2022-12-20 12:24:36 +0530

    new cpp

---
+++ spark.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+main()
+{
+  int a,b,c;
+  printf("enter the a");
+  scanf("%d",&a);
+  
+  printf("enter the b");
+  scanf("%d",b);
+  c=a+b;
+  printf("add=%d",c);
+    
+  
+}
